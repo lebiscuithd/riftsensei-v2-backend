deb2b8e1824932aef7ffefa063ff130fca34aa0e
lebiscuithd <72382518+lebiscuithd@users.noreply.github.com>
2021-02-05 14:49:17 +0100

    init:ads

--- database/migrations/2021_02_05_131907_create_ads_table.php
+++ database/migrations/2021_02_05_131907_create_ads_table.php
@@ -15,7 +15,19 @@ class CreateAdsTable extends Migration
     {
         Schema::create('ads', function (Blueprint $table) {
             $table->id();
+            $table->integer('coach_id');
+            $table->foreign('coach_id')->references('id')->on('users');
+            $table->dateTime('coaching_date');
+            $table->string('description');
+            $table->tinyInteger('ad_rating', 0, 5)->nullable();
+            $table->tinyInteger('duration');
+            $table->tinyInteger('hourly_rate');
+            $table->integer('total_price');
             $table->timestamps();
+            $table->enum('status', ['available', 'pending', 'finished', 'rated']);
+            $table->tinyInteger('student_id')->nullable();
+            $table->foreign('student_id')->references('id')->on('users');
+            $table->string('comments')->nullable();
         });
     }
 
