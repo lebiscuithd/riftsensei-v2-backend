14cefb50e45cfa8167738ee77fd7052cbef793d2
sg <xa>
2021-02-05 14:50:27 +0100

    'create:AdModel - fillable'

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Ad extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'coach_id',
+        'student_id',
+        'coaching_date',
+        'description',
+        'status',
+        'ad_rating',
+        'duration',
+        'hourly_rate',
+        'comments'
+    ];
+
 }
