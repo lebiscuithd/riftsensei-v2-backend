fc94f166b1a2f5da23352fd51cdd38331162f0c2
Alex <alexandre.macrel@epitech.eu>
2021-02-12 13:01:26 +0100

    Feat:ProductSeeder/ Refactor:Migration Product Table

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -13,6 +14,27 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('products')->insert([
+            [
+                'id' => 1,
+                'Quantity' => 100,
+                'Cost' => 4.99
+            ],
+            [
+                'id' => 2,
+                'Quantity' => 300,
+                'Cost' => 12.99
+            ],
+            [
+                'id' => 3,
+                'Quantity' => 500,
+                'Cost' => 19.99
+            ],
+            [
+                'id' => 4,
+                'Quantity' => 1000,
+                'Cost' => 34.99
+            ]
+    ]);
     }
 }
