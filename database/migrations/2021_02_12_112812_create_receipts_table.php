71de6ec3eea8898cbff26c192560b55c38108415
lebiscuithd <72382518+lebiscuithd@users.noreply.github.com>
2021-02-12 13:03:47 +0100

    feat:receipt

--- database/migrations/2021_02_12_112812_create_receipts_table.php
+++ database/migrations/2021_02_12_112812_create_receipts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserLanesTable extends Migration
+class CreateReceiptsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUserLanesTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_lanes', function (Blueprint $table) {
+        Schema::create('receipts', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('lane_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('lane_id')->references('id')->on('lanes')->onDelete('cascade');
             $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
         });
     }
 
@@ -30,6 +30,6 @@ class CreateUserLanesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_lanes');
+        Schema::dropIfExists('receipts');
     }
 }
