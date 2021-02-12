0af688057bd4fd83893d816b4181eaf4224d89d2
Alex <alexandre.macrel@epitech.eu>
2021-02-12 16:49:59 +0100

    test

--- database/migrations/2021_02_12_112756_create_products_table.php
+++ database/migrations/2021_02_12_112756_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->float('Quantity');
+            $table->integer('Quantity');
             $table->float('Cost');
             $table->string('image');
         });
