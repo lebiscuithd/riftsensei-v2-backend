17f214764ddeeb915148ba71c93360b2f834336a
Alex <alexandre.macrel@epitech.eu>
2021-02-12 14:30:15 +0100

    Feat:CoachResource

---
+++ app/Http/Resources/coachResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class coachResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+//        return parent::toArray($request);
+
+        return [
+            'id' => $this->id,
+            'username' => $this->username,
+            'email' => $this->email,
+            'verified_coach' => $this->verified_coach,
+            'avatar' => $this->avatar,
+            'coach_rating' => $this->coach_rating,
+
+            'rank' => $this->rank,
+            'lanes' => LaneResource::collection($this->lanes)
+        ];
+    }
+}
