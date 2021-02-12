a4e8deec579480c7f5b24a39510ed62eec5f2421
lebiscuithd <72382518+lebiscuithd@users.noreply.github.com>
2021-02-12 16:48:15 +0100

    feat:Ad-Controller

--- database/migrations/2021_02_12_112756_create_products_table.php
+++ database/migrations/2021_02_12_112756_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->integer('Quantity');
-            $table->integer('Cost');
+            $table->float('Cost');
             $table->string('image');
         });
     }
