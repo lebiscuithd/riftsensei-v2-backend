8f5fb07072207f357247e3ad4d2312c1e870a0d9
lebiscuithd <72382518+lebiscuithd@users.noreply.github.com>
2021-02-18 19:38:54 +0100

    refractor:ad-factory

--- database/factories/AdFactory.php
+++ database/factories/AdFactory.php
@@ -28,13 +28,13 @@ class AdFactory extends Factory
         return [
             'coach_id'=> User::all()->random()->id,
             'coaching_date' => $this->faker->dateTimeBetween($startDate = '+4 days', $endDate = '+1 year', $timezone = null),
-            'description' => $this->faker->text(200),
+            'description' => $this->faker->text(50),
             'status' => $this->faker->randomElement(['available' ,'pending', 'finished', 'rated']),
             'ad_rating' => $this->faker->numberBetween(0, 5),
             'duration' => $duration,
             'hourly_rate' => $hourly_rate,
             'student_id' => User::all()->random()->id,
-            'comments' => $this->faker->text(100),
+            'comments' => $this->faker->text(50),
             'total_price' => $total_price
         ];
     }
